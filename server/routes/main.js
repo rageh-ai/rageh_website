3bc5cd11efbb8ff22edb9330d46e53024a0db45c
rageh-ai <som3arageh@gmail.com>
2024-08-05 18:59:57 -0400

    some refining of index.ejs

---
+++ server/routes/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Routes
+router.get('', (req, res) =>{
+    res.render("index");
+});
+
+router.get('/about', (req, res) =>{
+    res.render("about");
+});
+
+router.get('/contact', (req, res) =>{
+    res.render('contact');
+});
+
+module.exports = router;
